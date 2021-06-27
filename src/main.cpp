b2618fed3618777fec752cc0de729aedafe87e7b
Ocean-git-hub <57902508+Ocean-git-hub@users.noreply.github.com>
2021-06-28 08:31:58 +0900

    comp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
 
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
+#define WIFI_SSID "TP-LINK_Extender_2.4GHz"
+#define WIFI_PASSWORD "6806147117"
+
+#define WEATHER_ENDPOINT "http://192.168.1.73/onecall.json"
+//#define WEATHER_ENDPOINT "https://api.openweathermap.org/data/2.5/onecall?lat=35.6907&lon=140.0216&exclude=current,minutely,daily,alerts&units=metric&appid=7aa81065e40021a999cec2ea8bc9d55c"
 
 #define ARDUINO_SERIAL_SPEED 9600
 
@@ -20,16 +25,46 @@ void setup() {
     configTzTime("JST-9", "ntp.nict.jp");
 
     arduinoSerial.begin(ARDUINO_SERIAL_SPEED);
+    Serial.begin(9600);
+}
+
+void sendTime() {
+    time_t t = time(nullptr);
+    struct tm *tm = localtime(&t);
+    arduinoSerial.write((char *) tm, sizeof(struct tm));
+}
+
+HTTPClient httpClient;
+WiFiClient client;
+
+StaticJsonDocument<20000> weatherJsonData;
+
+void sendWeather() {
+    httpClient.begin(client, WEATHER_ENDPOINT);
+    if (httpClient.GET() == HTTP_CODE_OK) {
+
+        DeserializationError error = deserializeJson(weatherJsonData, httpClient.getString());
+        if (error) {
+            Serial.println(error.f_str());
+            return;
+        }
+        Serial.println(weatherJsonData["hourly"][0]["weather"][0]["main"].as<const char *>());
+    }
+    httpClient.end();
 }
 
 void loop() {
-    if (arduinoSerial.available() > 0) {
-        if (arduinoSerial.read() == 1) {
-            time_t t = time(nullptr);
-            struct tm *tm = localtime(&t);
-            arduinoSerial.write((char *) tm, sizeof(struct tm));
-        } else {
-            arduinoSerial.print("Bad request");
+    if (Serial.available() > 0) {
+        switch (Serial.read()) {
+            case '1':
+                sendTime();
+                break;
+            case '2':
+                sendWeather();
+                break;
+            default:
+                arduinoSerial.print("Bad request");
+                break;
         }
     }
 }
